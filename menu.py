008282f36719cf0ca9328cf96be6b646a3db4fa8
jeevan0017 <jeevan.p@s.amity.edu>
2025-07-20 21:04:43 +0530

    Added another fuction code

--- menu.py
+++ menu.py
@@ -18,3 +18,12 @@ def add_task():
     task=input("Enter a new task:")
     todo_list.append(task)
     print(f"Task '{task}' added.")
+
+def remove_task():
+    view_tasks()
+    try:
+        task_num=int(input("Enter the task number to remove:"))
+        removed=todo_list.pop(task_num-1)
+        print(f"Task '{removed}' removed.")
+    except(Indexerror,valueerror):
+        print("Invalid task number.")
